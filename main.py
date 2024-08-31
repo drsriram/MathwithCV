8fe630f3aa8f8b7d98be9b0c68017851a0c43e35
drsriram <157253843+drsriram@users.noreply.github.com>
2024-08-31 15:54:17 +0530

    main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ with col2:
     st.title("Answer")
     output_text_area = st.subheader("")
 
-genai.configure(api_key="AIzaSyA1257SXz8YRlhU0hwsLpPWi02SeFBvZGE")
+genai.configure(api_key="AI*******************")
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Initialize the webcam to capture video
